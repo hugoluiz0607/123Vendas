0ffbb7a57bfb9a69f2a97985ec990c08ed126cce
hugoluiz0607 <hugoluiz@gmail.com>
2024-09-24 19:03:07 -0300

    feat(CrudVenda) - Campos adicionados FilialID em Venda e Desconto em Item Venda

---
+++ VendasAPI.Data/Migrations/20240924215353_featVendaIDFilialDescontoItem.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VendasAPI.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class featVendaIDFilialDescontoItem : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "FilialId",
+                table: "Vendas",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Desconto",
+                table: "ItensVenda",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FilialId",
+                table: "Vendas");
+
+            migrationBuilder.DropColumn(
+                name: "Desconto",
+                table: "ItensVenda");
+        }
+    }
+}
